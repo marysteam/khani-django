6dab68756bbec54945070a9d6cd0d663559d4b98
muniba <dm.youngs01@gmail.com>
2025-08-30 22:04:36 +0500

    after deleting folders

--- core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def home(request):
-    context = {"title": "Home"}
-    return render(request, "home.html", context)
