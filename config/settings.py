2e2006a23200a8b5f1501e3b21b0363f0d0a8cd1
muniba <dm.youngs01@gmail.com>
2025-08-30 19:24:42 +0500

    commit

--- config/settings.py
+++ config/settings.py
@@ -73,10 +73,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# Use the dummy backend to disable database usage. Any DB operation will raise
+# an ImproperlyConfigured error. Remove migrate steps in CI/deploy when using this.
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy',
     }
 }
 
@@ -119,6 +120,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
